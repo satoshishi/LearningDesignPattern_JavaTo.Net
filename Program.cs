b5eadc4379684529b9eefd3fa1bc21ce5ad63d51
fujisawa_satoshi <satoshi1327@gmail.com>
2021-03-14 11:28:48 +0900

    リファクタリング

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ISampleGenerater generater = new IteratorSample2();
+        generater.generateSample();
+
+        return;
+    }
+}
