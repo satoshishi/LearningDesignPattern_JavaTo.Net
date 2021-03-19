b57ddf4a88df4b30fafbc6c9d7cb6ccd70bfb341
satoshishi <satoshi1327@gmail.com>
2021-03-19 12:00:47 +0900

    adapterのsample3点

---
+++ Adapter/Unity/Main.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Adapter.Sample1;
+using Adapter;
+
+namespace Adapter
+{
+    public class Main : MonoBehaviour
+    {
+        private IPositionProvider PositionProvider
+        {
+            get;
+            set;
+        } = null;
+
+        [SerializeField]
+        private LerpPositionProvider m_lerpPositionProv;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            PositionProvider = new CSVPositionProvider((Resources.Load("Coordinates") as TextAsset).text);
+            //PositionProvider = m_lerpPositionProv;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (PositionProvider.TryGetPosition(out Vector3 pos))
+                this.transform.position = pos;
+        }
+    }
+}
---
+++ Adapter/Unity/PositionCSVRecorder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class PositionCSVRecorder : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_target;
+
+    private List<Vector3> Coordinates = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+            WriteCSV();
+
+        Coordinates.Add(m_target.position);
+    }
+
+    private void WriteCSV()
+    {
+        var sw = new StreamWriter(Application.dataPath+"/Coordinates.csv", true, Encoding.GetEncoding("Shift_JIS"));
+
+        foreach(Vector3 coordinate in Coordinates)
+            sw.WriteLine($"{coordinate.x},{coordinate.y},{coordinate.z}");        
+    }
+}
