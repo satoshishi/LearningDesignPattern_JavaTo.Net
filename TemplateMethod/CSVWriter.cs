c650ba5c83e2190ea9d6af9d2b8f1ec3fae19c65
satoshishi <satoshi1327@gmail.com>
2021-03-28 02:53:22 +0900

    サンプル

---
+++ TemplateMethod/CSVWriter.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TemplateMethod.Sample1
+{
+    public class CSVWriter : AbstractWriter
+    {
+        protected override void Output(string text)
+        {
+            var sw = new StreamWriter(Application.dataPath + "/Sample.csv", true, Encoding.GetEncoding("Shift_JIS"));
+
+            sw.Write(text);
+            sw.Close();
+        }
+
+        protected override string ToString<T>(List<T> datas)
+        {
+            string res = string.Empty;
+
+            foreach (T data in datas)
+                res += $"{data.ToString()}\n";
+
+            return res;
+        }
+    }
+
+    public class JsonWriter : AbstractWriter
+    {
+        [System.Serializable]
+        public class JsonData<T>
+        {
+            public List<T> datas;
+        }
+
+        protected override void Output(string text)
+        {
+            var sw = new StreamWriter(Application.dataPath + "/Sample.json", true, Encoding.GetEncoding("Shift_JIS"));
+
+            sw.Write(text);
+            sw.Close();
+        }
+
+        protected override string ToString<T>(List<T> datas)
+        {
+            return JsonUtility.ToJson(
+                new JsonData<T>()
+                {
+                    datas = datas
+                }
+            );
+        }
+    }
+}
