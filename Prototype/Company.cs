1bf01af905cea20235af618b5154eb76e8efb267
satoshishi <satoshi1327@gmail.com>
2021-04-18 09:48:09 +0900

    ProtoTypeパターンのサンプル

---
+++ Prototype/Company.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace ProtoType.Sample1
+{
+    public class Company
+    {
+        private Dictionary<DateTime,HololiverBase> livers;
+
+        public Company() => livers = new Dictionary<DateTime, HololiverBase>();
+
+        public void Register(DateTime date,HololiverBase liver) => livers.Add(date,liver);
+
+        public HololiverBase Create(DateTime date) => livers[date].Clone();
+
+    }
+}
---
+++ Prototype/HololiverBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace ProtoType.Sample1
+{
+    public abstract class HololiverBase
+    {
+        protected string LiveStreamingGameTitle;
+
+        public abstract string Live();
+
+        public HololiverBase Clone() => (HololiverBase)MemberwiseClone();
+
+        public HololiverBase(string liveStreamingGameTitle) => LiveStreamingGameTitle = liveStreamingGameTitle;
+    }
+}
